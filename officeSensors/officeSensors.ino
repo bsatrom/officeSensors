82c5ad53588b8ad6ddac21622ed5b89063b0e516
Brandon Satrom <bsatrom@gmail.com>
2015-04-02 22:31:08 -0500

    finished code for logging sensors to Wolfram DataDrop every five seconds

--- officeSensors/officeSensors.ino
+++ officeSensors/officeSensors.ino
@@ -5,6 +5,7 @@
 // Includes for WiFi Connectivity
 #include <SPI.h>
 #include <SFE_CC3000.h>
+#include <SFE_CC3000_Client.h>
 
 MPL3115A2 myPressure; //Create an instance of the pressure sensor
 HTU21D myHumidity; //Create an instance of the humidity sensor
@@ -12,14 +13,13 @@ HTU21D myHumidity; //Create an instance of the humidity sensor
 //Hardware pin definitions for weather shield
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // digital I/O pins
-const byte STAT1 = 7;
-const byte STAT2 = 8;
+#define STAT2 8
 
 // analog I/O pins
-const byte REFERENCE_3V3 = A3;
-const byte LIGHT = A1;
-const byte BATT = A2;
-const byte WDIR = A0;
+#define REFERENCE_3V3 A3
+#define LIGHT A1
+#define BATT A2
+#define WDIR A0
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 //Hardware pins for CC3000 WiFi chip
@@ -31,27 +31,24 @@ const byte WDIR = A0;
 #define IP_ADDR_LEN     4   // Length of IP address in bytes
 
 // Constants
-unsigned int ap_security = WLAN_SEC_WPA2; // Security of network
-unsigned int timeout = 30000;             // Milliseconds
+#define ap_security WLAN_SEC_WPA2
+#define timeout 30000
 
 //Global Variables
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 SFE_CC3000 wifi = SFE_CC3000(CC3000_INT, CC3000_EN, CC3000_CS);
+SFE_CC3000_Client client = SFE_CC3000_Client(wifi);
 
-long lastSecond; //The millis counter to see when a second rolls by
-byte seconds; //When it hits 60, increase the current minute
-byte seconds_2m; //Keeps track of the "wind speed/dir avg" over last 2 minutes array of data
-byte minutes; //Keeps track of where we are in various arrays of data
-byte minutes_10m; //Keeps track of where we are in wind gust/dir over last 10 minutes array of data
+// Remote Server for posting temperature data
+#define server "datadrop.wolframcloud.com"
+
+unsigned long lastConnectionTime;
 
 //These are all the weather values that wunderground expects:
 float humidity = 0; // [%]
 float tempf = 0; // [temperature F]
-//float baromin = 30.03;// [barom in] - It's hard to calculate baromin locally, do this in the agent
 float pressure = 0;
-//float dewptf; // [dewpoint F] - It's hard to calculate dewpoint locally, do this in the agent
 
-float batt_lvl = 11.8; //[analog value from 0 to 1023]
 float light_lvl = 455; //[analog value from 0 to 1023]
 
 void setup()
@@ -65,8 +62,6 @@ void setup()
 
   if ( wifi.init() ) {
     Serial.println("CC3000 initialization complete");
-  } else {
-    Serial.println("Something went wrong during CC3000 init!");
   }
   
   // Connect to WiFi network stored in non-volatile memory
@@ -91,9 +86,6 @@ void setup()
     Serial.println();
   }
   
-  Serial.println("Weather Shield starting up...");
-
-  pinMode(STAT1, OUTPUT); //Status LED Blue
   pinMode(STAT2, OUTPUT); //Status LED Green
   
   pinMode(REFERENCE_3V3, INPUT);
@@ -108,47 +100,84 @@ void setup()
   //Configure the humidity sensor
   myHumidity.begin();
 
-  seconds = 0;
-  lastSecond = millis();
+  lastConnectionTime = millis();
 
+  Serial.println();
   Serial.println("Weather Shield online!");
 }
 
 void loop()
-{
-  //Keep track of which minute it is
-  if(millis() - lastSecond >= 1000)
-  {
-    digitalWrite(STAT1, HIGH); //Blink stat LED
-    
-    //Report all readings every second
-    printWeather();
-
-    digitalWrite(STAT1, LOW); //Turn off stat LED
+{  
+  while (client.available()) {   
+    char c = client.read();
+    if (c) {
+      Serial.print(c);
+    }
   }
+    
+  if(millis() - lastConnectionTime > 5000)
+  {
+    digitalWrite(STAT2, HIGH); //Blink stat LED
+    
+    calcWeather();
+    printWeather();
+    postWeather();
 
-  delay(5000);
+    digitalWrite(STAT2, LOW); //Turn off stat LED
+  }
 }
 
-//Calculates each of the variables that wunderground is expecting
+void postWeather() {
+  client.stop();
+  
+  if (client.connect(server, 80)) {
+    Serial.println("Logging sensor data...");
+  
+    client.print("GET /api/v1.0/Add?bin=3_zE53-m&humidity=");
+    client.print(humidity);
+    client.print("&temp=");
+    client.print(tempf);
+    client.print("&pressure=");
+    client.print(pressure);
+    client.print("&light=");
+    client.print(light_lvl);
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(server);
+    client.println("Cache-Control: no-cache");
+    client.println();
+    
+    Serial.println("Log sensors complete");
+    lastConnectionTime = millis(); 
+  } else {
+    Serial.println("connection failed");
+  }
+}
+
+void printWeather()
+{
+  Serial.println();
+  Serial.print("humidity=");
+  Serial.print(humidity, 1);
+  Serial.print(",tempf=");
+  Serial.print(tempf, 1);
+  Serial.print(",pressure=");
+  Serial.print(pressure, 2);
+  Serial.print(",light_lvl=");
+  Serial.println(light_lvl, 2);
+}
+
 void calcWeather()
 {
   //Calc humidity
   humidity = myHumidity.readHumidity();
-  //float temp_h = myHumidity.readTemperature();
-  //Serial.print(" TempH:");
-  //Serial.print(temp_h, 2);
-
+  
   //Calc tempf from pressure sensor
   tempf = myPressure.readTempF();
-  //Serial.print(" TempP:");
-  //Serial.print(tempf, 2);
-
+  
   //Calc pressure
   pressure = myPressure.readPressure();
-
-  //Calc dewptf
-
+ 
   //Calc light level
   light_lvl = get_light_level();
 }
@@ -166,35 +195,4 @@ float get_light_level()
   lightSensor = operatingVoltage * lightSensor;
   
   return(lightSensor);
-}
-
-//Prints the various variables directly to the port
-//I don't like the way this function is written but Arduino doesn't support floats under sprintf
-void printWeather()
-{
-  calcWeather(); //Go calc all the various sensors
-
-  Serial.println();
-  Serial.print("humidity=");
-  Serial.print(humidity, 1);
-  Serial.print(",tempf=");
-  Serial.print(tempf, 1);
-  Serial.print(",pressure=");
-  Serial.print(pressure, 2);
-  Serial.print(",light_lvl=");
-  Serial.print(light_lvl, 2);
-  Serial.print(",");
-  Serial.println("#");
-}
-
-// Print out an IP Address in human-readable format
-void printIPAddr(unsigned char ip_addr[]) {
-  int i;
-  
-  for (i = 0; i < IP_ADDR_LEN; i++) {
-    Serial.print(ip_addr[i]);
-    if ( i < IP_ADDR_LEN - 1 ) {
-      Serial.print(".");
-    }
-  }
 }
